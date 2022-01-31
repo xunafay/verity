47bb70a078f2d63b7ba43ef59b6f6ea1d15f3624
Xunafay <me@xunafay.io>
2022-01-31 15:24:41 +0100

    feat(ticket): validate body part requirements

--- src/ticket-manager.ts
+++ src/ticket-manager.ts
@@ -6,4 +6,15 @@ export class TicketManager {
     sortTickets(): Ticket[] {
         return this.room.memory.tickets.sort((a, b) => b.priority - a.priority);
     }
+
+    validTicketsForCreep(creep: Creep): Ticket[] {
+        return this.sortTickets().filter(ticket => {
+            const unique_body_parts: Array<BodyPartConstant> = [... new Set(creep.body.map(bodyPart => bodyPart.type))];
+            return ticket.requirements.every(part => unique_body_parts.includes(part));
+        });
+    }
+
+    unnasignedTickets(): Ticket[] {
+        return this.sortTickets().filter(ticket => ticket.assignees.length < ticket.maxAssignees);
+    }
 }
